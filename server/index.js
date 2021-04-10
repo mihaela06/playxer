33ba2837f0e837f4366e59d689a3b0d627c3d4ab
mihaela06 <62171914+mihaela06@users.noreply.github.com>
2021-04-11 01:16:14 +0300

    Updated config file
    Changed path to key.js

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ const config = require("./config/key");
 
 const mongoose = require("mongoose");
 const connect = mongoose
-  .connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,30 +33,30 @@ app.use("/api/users", require("./routes/users"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+    // Set static folder
+    // All the javascript and css files will be read and served from this folder
+    app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+    // index.html for all page routes    html or routing and naviagtion
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
 }
 
 const port = process.env.PORT || 5050;
 
 if (process.env.NODE_ENV === "production") {
-  var http = require("http");
-  var https = require("https");
-  var fs = require("fs");
-  var enforce = require("express-sslify");
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  http.createServer(app).listen(port, () => {
-    console.log(`Server Listening on ${port}`);
-  });
+    var http = require("http");
+    var https = require("https");
+    var fs = require("fs");
+    var enforce = require("express-sslify");
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    http.createServer(app).listen(port, () => {
+        console.log(`Server Listening on ${port}`);
+    });
 } else {
-  console.log("DEVELOPMENT MODE");
-  app.listen(port, () => {
-    console.log(`Server Listening on ${port}`);
-  });
+    console.log("DEVELOPMENT MODE");
+    app.listen(port, () => {
+        console.log(`Server Listening on ${port}`);
+    });
 }
